c52edb0643cabe4cb90ec02272c05f6bf10bef6b
Abdellah Alaouchiche <abdou.sam7@gmail.com>
2022-01-05 11:31:42 +0100

    ransac.h include unorderd_set

--- src/quiz/ransac/ransac.h
+++ src/quiz/ransac/ransac.h
@@ -1,6 +1,8 @@
 #ifndef RANSAC_H
 #define RANSAC_H
 
+#include <unordered_set>
+
 std::unordered_set<int> Ransac3D(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, int maxIterations, float distanceTol);
 
 std::unordered_set<int> Ransac2D(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, int maxIterations, float distanceTol);
